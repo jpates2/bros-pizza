5bc3dba10e373ba0e15e467e695dedeac087a20f
Jess Pates <jesspates@hotmail.co.uk>
2023-09-25 17:59:23 +0100

    Meal structure and style updates

--- src/components/AvailableMeals.js
+++ src/components/AvailableMeals.js
@@ -3,9 +3,44 @@ import classes from "./AvailableMeals.module.css";
 
 import MenuItem from "./MenuItem";
 
+const DUMMY_MEALS = [
+  {
+    id: "p1",
+    name: "MARGHERITA",
+    description: "Tomato, mozzarella, basil",
+    price: 8
+  },
+  {
+    id: "p2",
+    name: "PEPPERONI",
+    description: "Tomato, mozzarella, pepperoni, basil",
+    price: 8
+  },
+  {
+    id: "p3",
+    name: "VEGAN",
+    description: "Tomato, vegan mozzarella, basil",
+    price: 8
+  }
+]
+
 const AvailableMeals = (props) => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MenuItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ))
+
   return (
-    <MenuItem />
+    <div>
+      <ul className={classes["available-meals__list"]}>
+        {mealsList}
+      </ul>
+    </div>
   )
 }
 
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,7 +3,15 @@ import classes from "./MenuItem.module.css"
 
 const MenuItem = (props) => {
   return (
-    <div>Sample item</div>
+    <li className={classes["menu__item"]}>
+      <div className={classes["menu__item-text"]}>
+        <h3 className={classes["menu__item-name"]}>{props.name}</h3>
+        <div className={classes["menu__item-description"]}>{props.description}</div>
+      </div>
+      <div>
+        <div className={classes["menu__item-price"]}>{props.price}</div>
+      </div>
+    </li>
   )
 }
 
