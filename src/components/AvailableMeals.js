18c85ad499ec8bbe20f0f14410248dba7d2d5637
Jess Pates <jesspates@hotmail.co.uk>
2023-09-25 23:16:41 +0100

    menu tab logic for showing menu items

--- src/components/AvailableMeals.js
+++ src/components/AvailableMeals.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AvailableMeals.module.css";
 
 import MenuItem from "./MenuItem";
 
-const DUMMY_MEALS = [
+const DUMMY_STARTERS = [
+  {
+    id: "s1",
+    name: "BRUSCHETTA",
+    description: "Tomato, mozzarella, basil",
+    price: 8
+  },
+  {
+    id: "s2",
+    name: "ARANCINI",
+    description: "Tomato, mozzarella, pepperoni, basil",
+    price: 8
+  },
+  {
+    id: "s3",
+    name: "BURRATA",
+    description: "Tomato, vegan mozzarella, basil",
+    price: 8
+  },
+  {
+    id: "s4",
+    name: "PARMA HAM",
+    description: "Tomato, garlic, oregano",
+    price: 8
+  }
+]
+
+const DUMMY_PIZZAS = [
   {
     id: "p1",
     name: "MARGHERITA",
@@ -30,8 +57,35 @@ const DUMMY_MEALS = [
   }
 ]
 
+const DUMMY_DESSERTS = [
+  {
+    id: "d1",
+    name: "TIRAMISU",
+    description: "Tomato, mozzarella, basil",
+    price: 8
+  },
+  {
+    id: "d2",
+    name: "CANNOLI",
+    description: "Tomato, mozzarella, pepperoni, basil",
+    price: 8
+  },
+  {
+    id: "d3",
+    name: "PROFITEROLES",
+    description: "Tomato, vegan mozzarella, basil",
+    price: 8
+  },
+  {
+    id: "d4",
+    name: "LEMON MERINGUE PIE",
+    description: "Tomato, garlic, oregano",
+    price: 8
+  }
+]
+
 const AvailableMeals = (props) => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const starterList = DUMMY_STARTERS.map((meal) => (
     <MenuItem
       id={meal.id}
       key={meal.id}
@@ -41,10 +95,42 @@ const AvailableMeals = (props) => {
     />
   ))
 
+  const pizzaList = DUMMY_PIZZAS.map((meal) => (
+    <MenuItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ))
+
+  const dessertList = DUMMY_DESSERTS.map((meal) => (
+    <MenuItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ))
+
+
+  let menu;
+  if (props.menuToDisplay === "starterList") {
+    menu = starterList;
+  }
+  if (props.menuToDisplay === "pizzaList") {
+    menu = pizzaList;
+  }
+  if (props.menuToDisplay === "dessertList") {
+    menu = dessertList;
+  }
+
   return (
     <div>
       <ul className={classes["available-meals__list"]}>
-        {mealsList}
+        {menu}
       </ul>
     </div>
   )
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Menu.module.css"
 
 import AvailableMeals from "./AvailableMeals";
 
-const Menu = () => {
+const Menu = (props) => {
+  const [menuSelection, setMenuSelection] = useState("pizzaList");
+
+  const starterMenuHandler = () => {
+    setMenuSelection("starterList");
+  }
+
+  const pizzaMenuHandler = () => {
+    setMenuSelection("pizzaList");
+  }
+
+  const dessertMenuHandler = () => {
+    setMenuSelection("dessertList");
+  }
+
   return (
     <section className={classes["menu__section"]}>
       <h1 className={classes["menu__header"]}>MENU</h1>
       <div className={classes["menu__tabs"]}>
-        <div className={classes["menu__tab"]}>STARTERS</div>
-        <div className={classes["menu__tab"]}>PIZZAS</div>
-        <div className={classes["menu__tab"]}>DESSERTS</div>
+        <div onClick={starterMenuHandler} className={classes["menu__tab"]}>STARTERS</div>
+        <div onClick={pizzaMenuHandler} className={classes["menu__tab"]}>PIZZAS</div>
+        <div onClick={dessertMenuHandler} className={classes["menu__tab"]}>DESSERTS</div>
       </div>
-      <AvailableMeals />
+      <AvailableMeals menuToDisplay={menuSelection} />
     </section>
   )
 }
