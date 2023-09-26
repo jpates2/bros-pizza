40c34ff02c5314c101c1b31b55ad90d1799cf5da
Jess Pates <jesspates@hotmail.co.uk>
2023-09-26 17:55:05 +0100

    WhoSection component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Layout/Header";
 import Nav from "./Layout/Nav";
 import Menu from "./components/Menu";
+import WhoSection from "./Layout/WhoSection";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Nav />
         <Menu />
+        <WhoSection />
       </main>
     </React.Fragment>
   );
---
+++ src/Layout/WhoSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WhoSection = () => {
+
+}
+
+export default WhoSection;
