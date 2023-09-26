d41cfe6f1064661951cdfb56b23d4ce0cb0a78d1
Jess Pates <jesspates@hotmail.co.uk>
2023-09-26 22:20:33 +0100

    WhoSectionImages component added

--- src/Layout/WhoSection.js
+++ src/Layout/WhoSection.js
@@ -1,7 +1,15 @@
 import React from "react";
+import classes from "./WhoSection.module.css";
+import WhoSectionImages from "./WhoSectionImages";
 
 const WhoSection = () => {
-
+  return (
+    <section className={classes["who__section"]}>
+      <h1 className={classes["who__header"]}>WHO WE ARE</h1>
+      <WhoSectionImages />
+      <p className={classes["who__text"]}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nostrum tenetur animi fugiat aspernatur dolorum veniam, unde, blanditiis facilis accusantium eum quo nam nesciunt odio illo quis quia exercitationem, corrupti iusto facere? Accusamus consequatur odio nesciunt soluta quos libero excepturi aspernatur. Magnam nisi, fuga fugit modi corrupti ad deleniti saepe?</p>
+    </section>
+  )
 }
 
 export default WhoSection;
---
+++ src/Layout/WhoSectionImages.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./WhoSectionImages.module.css";
+
+const WhoSectionImages = () => {
+
+}
+
+export default WhoSectionImages;
