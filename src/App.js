104238743372e72108bde059428318c2fe55d4c2
Jess Pates <jesspates@hotmail.co.uk>
2023-09-27 21:04:20 +0100

    WhereSection added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Layout/Header";
 import Nav from "./Layout/Nav";
 import Menu from "./components/Menu";
 import WhoSection from "./Layout/WhoSection";
+import WhereSection from "./Layout/WhereSection";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Nav />
         <Menu />
         <WhoSection />
+        <WhereSection />
       </main>
     </React.Fragment>
   );
---
+++ src/Layout/WhereSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./WhereSection.module.css";
+
+const WhereSection = () => {
+  return (
+    <section className={classes["where__section"]}>
+      <h1 className={classes["where__header"]}>WHERE WE ARE</h1>
+      <div className={classes["where__cards-container"]}>
+
+      </div>
+    </section>
+  )
+}
+
+export default WhereSection;
