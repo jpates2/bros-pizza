2f926d399e16d865d55435b95145f49e3318a9a6
Jess Pates <jesspates@hotmail.co.uk>
2023-09-27 21:07:43 +0100

    WhereCard component added

---
+++ src/Layout/WhereCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./WhereCard.module.css";
+
+const WhereCard = () => {
+  return (
+    <div>
+      text
+    </div>
+  )
+}
+
+export default WhereCard;
--- src/Layout/WhereSection.js
+++ src/Layout/WhereSection.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./WhereSection.module.css";
+import WhereCard from "./WhereCard";
 
 const WhereSection = () => {
   return (
     <section className={classes["where__section"]}>
       <h1 className={classes["where__header"]}>WHERE WE ARE</h1>
       <div className={classes["where__cards-container"]}>
-
+        <WhereCard />
       </div>
     </section>
   )
