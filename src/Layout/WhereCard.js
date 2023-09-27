c07c69b1f40b76715286381209af0e3b6321cfb7
Jess Pates <jesspates@hotmail.co.uk>
2023-09-27 22:15:35 +0100

    WhereCard and Section styling

--- src/Layout/WhereCard.js
+++ src/Layout/WhereCard.js
@@ -8,8 +8,8 @@ const WhereCard = (props) => {
     <div className={classes["where__card"]}>
       <h3 className={classes["where__card-title"]}>{props.area}</h3>
       <p className={classes["where__card-text"]}>{props.street}</p>
-      <p className={classes["where__card-text"]}>{props.postcode}</p>
-      <a href={mapLink} target="_blank" rel="noreferrer" className={classes["where__card-text"]}>View location</a>
+      <p className={`${classes["where__card-text"]} ${classes["where__card-text-bottom"]}`}>{props.postcode}</p>
+      <a href={mapLink} target="_blank" rel="noreferrer" className={classes["where__card-link"]}>View location</a>
     </div>
   )
 }
