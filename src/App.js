efe4d118580081b2c474f64de823e26af92254ef
Jess Pates <jesspates@hotmail.co.uk>
2023-09-29 18:51:58 +0100

    Where and Who folders created

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./Layout/Header";
 import Nav from "./Layout/Nav";
 import Menu from "./components/Menu";
-import WhoSection from "./Layout/WhoSection";
-import WhereSection from "./Layout/WhereSection";
+import WhoSection from "./Who/WhoSection";
+import WhereSection from "./Where/WhereSection";
 
 function App() {
   return (
