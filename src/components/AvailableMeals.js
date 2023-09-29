21dafee9f96f57ef365f34bd235679d5b9e35587
Jess Pates <jesspates@hotmail.co.uk>
2023-09-29 14:16:02 +0100

    Extra meals added, grid CSS for meals amended

--- src/components/AvailableMeals.js
+++ src/components/AvailableMeals.js
@@ -7,25 +7,37 @@ const DUMMY_STARTERS = [
   {
     id: "s1",
     name: "BRUSCHETTA",
-    description: "Tomato, mozzarella, basil",
+    description: "Toasted focaccia, tomato, basil, garlic oil",
     price: 8
   },
   {
     id: "s2",
-    name: "ARANCINI",
-    description: "Tomato, mozzarella, pepperoni, basil",
+    name: "MEATY ARANCINI",
+    description: "Stuffed and fried rice balls, beef ragu, parmesan",
     price: 8
   },
   {
     id: "s3",
-    name: "BURRATA",
-    description: "Tomato, vegan mozzarella, basil",
+    name: "VEGGIE ARANCINI",
+    description: "Stuffed and fried rice balls, mushrooms, taleggio",
     price: 8
   },
   {
     id: "s4",
+    name: "BURRATA",
+    description: "Burrata di Puglia, marinara sauce, pesto",
+    price: 8
+  },
+  {
+    id: "s5",
     name: "PARMA HAM",
-    description: "Tomato, garlic, oregano",
+    description: "Prosciutto di Parma DOP, warm focaccia",
+    price: 8
+  },
+  {
+    id: "s6",
+    name: "MEATBALLS",
+    description: "Pork meatballs, marinara sauce, parmesan",
     price: 8
   }
 ]
@@ -33,53 +45,89 @@ const DUMMY_STARTERS = [
 const DUMMY_PIZZAS = [
   {
     id: "p1",
+    name: "MARINARA",
+    description: "Tomato, garlic, oregano",
+    price: 6
+  },
+  {
+    id: "p2",
     name: "MARGHERITA",
     description: "Tomato, mozzarella, basil",
     price: 8
   },
   {
-    id: "p2",
-    name: "PEPPERONI",
-    description: "Tomato, mozzarella, pepperoni, basil",
-    price: 8
-  },
-  {
     id: "p3",
-    name: "VEGAN",
-    description: "Tomato, vegan mozzarella, basil",
-    price: 8
+    name: "PEPPERONI",
+    description: "Tomato, mozzarella, pepperoni",
+    price: 10
   },
   {
     id: "p4",
-    name: "MARINARA",
-    description: "Tomato, garlic, oregano",
+    name: "NDUJA",
+    description: "Tomato, mozzarella, nduja, salami, honey",
+    price: 11
+  },
+  {
+    id: "p5",
+    name: "AUBERGINE PARM",
+    description: "Tomato, mozzarella, aubergine, parmesan, breadcrumbs",
+    price: 10
+  },
+  {
+    id: "p6",
+    name: "PARMA",
+    description: "Tomato, mozzarella, parma ham, rocket, parmesan",
+    price: 12
+  },
+  {
+    id: "p7",
+    name: "BIANCA",
+    description: "Ricotta, mozzarella, garlic, oregano",
+    price: 11
+  },
+  {
+    id: "p8",
+    name: "BUFFALO",
+    description: "Tomato, buffalo mozzarella, sundried tomatoes, pesto",
+    price: 14
+  },
+  {
+    id: "p9",
+    name: "VEGANDUJA",
+    description: "Tomato, vegan mozzarella, vegan nduja, mushrooms",
+    price: 10
+  },
+  {
+    id: "p10",
+    name: "PARMA",
+    description: "Tomato, vegan mozzarella, roasted broccoli, charred aubergine, caramalised onions",
     price: 8
-  }
+  },
 ]
 
 const DUMMY_DESSERTS = [
   {
     id: "d1",
     name: "TIRAMISU",
-    description: "Tomato, mozzarella, basil",
+    description: "Sponge, coffee, chocolate, cream, rum",
     price: 8
   },
   {
     id: "d2",
     name: "CANNOLI",
-    description: "Tomato, mozzarella, pepperoni, basil",
+    description: "Ricotta filling, pistachio, dark chocolate",
     price: 8
   },
   {
     id: "d3",
     name: "PROFITEROLES",
-    description: "Tomato, vegan mozzarella, basil",
+    description: "Choux pastry, cream, chocolate sauce, hazelnuts",
     price: 8
   },
   {
     id: "d4",
     name: "LEMON MERINGUE PIE",
-    description: "Tomato, garlic, oregano",
+    description: "Sweet pastry, lemon curd, whipped meringue",
     price: 8
   }
 ]
