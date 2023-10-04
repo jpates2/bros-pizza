d0ed4696b903643af4c7f4f3f3e8fb6a1afed7ea
Jess Pates <jesspates@hotmail.co.uk>
2023-10-04 20:43:45 +0100

    home link added to order page

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,7 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 function OrderPage() {
- return (
-  <div>Order</div>
- )
+  return (
+    <React.Fragment>
+      <button><Link to="/">HOME</Link></button>
+      <div>Order</div>
+    </React.Fragment>
+  )
 }
 
 export default OrderPage;
