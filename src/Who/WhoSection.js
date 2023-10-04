7e1ad38f772f0a201205d80dc205db76558e62bc
Jess Pates <jesspates@hotmail.co.uk>
2023-10-04 20:31:41 +0100

    text for who we are section

--- src/Who/WhoSection.js
+++ src/Who/WhoSection.js
@@ -7,7 +7,9 @@ const WhoSection = () => {
     <section className={classes["who__section"]}>
       <h1 className={classes["who__header"]}>WHO WE ARE</h1>
       <WhoSectionImages />
-      <p className={classes["who__text"]}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nostrum tenetur animi fugiat aspernatur dolorum veniam, unde, blanditiis facilis accusantium eum quo nam nesciunt odio illo quis quia exercitationem, corrupti iusto facere? Accusamus consequatur odio nesciunt soluta quos libero excepturi aspernatur. Magnam nisi, fuga fugit modi corrupti ad deleniti saepe?</p>
+      <p className={classes["who__text"]}>Welcome to Bros Pizza, a slice of Italy right here in the heart of London!</p>
+      <p className={classes["who__text"]}>We're not your typical pizza joint – we're a bunch of friends who share an insatiable love for food and a passion for creating the perfect pizza. Our journey began when we embarked on a food-filled adventure to Italy, where we tasted some of the most mouthwatering pizzas imaginable. Inspired by the artistry and simplicity of Italian pizza-making, we decided to bring that same magic back to London.</p>
+      <p className={classes["who__text"]}>At Bros Pizza, we're all about using the finest ingredients, crafting each pie with love, and serving up a slice of happiness to our amazing customers. So, whether you're a fellow pizza enthusiast or just looking for a delicious meal with friends, come on over and join our pizza-loving family!</p>
     </section>
   )
 }
