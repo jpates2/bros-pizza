25eb4b4e1d246c15ee16d0382e4e08cda0e268b5
Jess Pates <jesspates@hotmail.co.uk>
2023-10-06 19:09:09 +0100

    order summary nav component added

---
+++ src/components/OrderSummary.js
@@ -0,0 +1,11 @@
+import classes from "./OrderSummary.module.css";
+
+const OrderSummary = () => {
+  return (
+    <div className={classes["order-summary__container"]}>
+      <h1 className={classes["order-summary__title"]}>ORDER SUMMARY</h1>
+    </div>
+  )
+}
+
+export default OrderSummary;
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import OrderNav from "../Layout/OrderNav";
+import OrderSummary from "../components/OrderSummary";
 
 function OrderPage() {
   return (
     <React.Fragment>
       <OrderNav />
+      <OrderSummary />
     </React.Fragment>
   )
 }
