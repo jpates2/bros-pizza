d15cf3ba100e1bce15610ae6e09771573a023a88
Jess Pates <jesspates@hotmail.co.uk>
2023-10-09 08:51:33 +0100

    cart styling updates

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,14 @@
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+  const price = `£${(props.price * props.amount).toFixed(2)}`
+
   return (
     <li className={classes["cart__list-item"]}>
       <div className={classes["cart__list-name"]}>
         {props.name.toUpperCase()}
       </div>
-      <div className={classes["cart__list-price"]}>{(props.price * props.amount).toFixed(2)}</div>
+      <div className={classes["cart__list-price"]}>{price}</div>
       <div className={classes["cart__list-button-container"]}>
         <button onClick={props.onRemove} className={`${classes["cart__list-button"]} ${classes["cart__list-button-minus"]}`}><p>-</p></button>
         <div>{props.amount}</div>
