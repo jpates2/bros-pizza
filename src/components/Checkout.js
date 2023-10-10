79590ea4b1af7280200ec1004c4f9e6b43d66e62
Jess Pates <jesspates@hotmail.co.uk>
2023-10-10 23:07:36 +0100

    checkout content and css

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import Modal from "../Layout/Modal";
 import CartContext from "../store/cart.context";
+import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
   const cartCtx = useContext(CartContext);
@@ -9,11 +10,19 @@ const Checkout = (props) => {
   const checkoutTotal = `£${cartCtx.total.toFixed(2)}`;
 
   return (
-    <Modal onClose={props.onClose} >
-      <div>COMPLETE ORDER</div>
-      <div>
-        <span>Total</span>
-        <span>{checkoutTotal}</span>
+    <Modal onClose={props.onClose}>
+      <div className={classes["checkout__container"]}>
+        <div className={classes["checkout__title"]}>COMPLETE ORDER</div>
+        <div className={classes["checkout__total"]}>
+          <div>Total</div>
+          <div>{checkoutTotal}</div>
+        </div>
+        <div>
+          <div className={classes["checkout__title"]}>DELIVERY</div>
+        </div>
+        <div>
+          <div className={classes["checkout__title"]}>PAYMENT</div>
+        </div>
       </div>
     </Modal>
   )
