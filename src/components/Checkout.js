05855890f5b0b69c33dd2b20af70f4bd307de8ef
Jess Pates <jesspates@hotmail.co.uk>
2023-10-10 22:34:27 +0100

    cart context added to checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+
 import Modal from "../Layout/Modal";
+import CartContext from "../store/cart.context";
 
 const Checkout = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const checkoutTotal = `£${cartCtx.total.toFixed(2)}`;
+
   return (
     <Modal onClose={props.onClose} >
-      <div>Checkout</div>
+      <div>COMPLETE ORDER</div>
+      <div>
+        <span>Total</span>
+        <span>{checkoutTotal}</span>
+      </div>
     </Modal>
   )
 }
