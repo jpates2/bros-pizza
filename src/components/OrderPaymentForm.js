d10843e0f98a2720117d557e8aa2da5ef1567bc9
Jess Pates <jesspates@hotmail.co.uk>
2023-10-13 17:54:55 +0100

    order payment form added

--- src/components/OrderPaymentForm.js
+++ src/components/OrderPaymentForm.js
@@ -1,6 +1,38 @@
+import React from "react";
+import classes from "./OrderPaymentForm.module.css";
+
 const OrderPaymentForm = () => {
+
+
+  let content = (
+    <form>
+      <div className={classes["payment__form"]}>
+        <div className={classes["payment__form-container"]}>
+          <div className={classes["payment__form-control"]}>
+            <label htmlFor="number">Card Number</label>
+            <input type="text" id="number" />
+          </div>
+          <div className={classes["payment__form-control"]}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={classes["payment__form-control"]}>
+            <label htmlFor="expiry">Expiry Date</label>
+            <input type="text" id="expiry" />
+          </div>
+          <div className={classes["payment__form-control"]}>
+            <label htmlFor="security">Security Number</label>
+            <input type="text" id="security" />
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+
   return (
-    <div>Payment form</div>
+    <React.Fragment>
+      {content}
+    </React.Fragment>
   )
 }
 
