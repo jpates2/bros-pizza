3b38a14dbe7bd470add9f1f013cd56a64b3d48d3
Jess Pates <jesspates@hotmail.co.uk>
2023-10-15 11:49:57 +0100

    footer links update

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
     <footer>
       <p className={classes["footer__text"]}>Created by Jess Pates</p>
       <div className={classes["footer__links"]}>
-        <a href="https://www.linkedin.com/in/jess-pates/" target="_blank" rel="noreferrer"><i className={`${classes["footer__link"]}`}><FontAwesomeIcon icon={faGithubSquare} /></i></a>
-        <a href="https://github.com/jpates2/" target="_blank" rel="noreferrer"><i className={`${classes["footer__link"]}`}><FontAwesomeIcon icon={faLinkedin} /></i></a>
+        <a href="https://www.linkedin.com/in/jess-pates/" target="_blank" rel="noreferrer"><i className={`${classes["footer__link"]}`}><FontAwesomeIcon icon={faLinkedin} /></i></a>
+        <a href="https://github.com/jpates2/bros-pizza" target="_blank" rel="noreferrer"><i className={`${classes["footer__link"]}`}><FontAwesomeIcon icon={faGithubSquare} /></i></a>
       </div>
       <div className={classes["footer__license"]}>SVG Credit: Vincent Le Moign, CC BY 4.0, via Wikimedia Commons. License <a href="https://creativecommons.org/licenses/by/4.0" target="_blank" rel="noreferrer"><u>here.</u></a>
       </div>
